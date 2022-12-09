c256e7767c6ce46b4a013e21f986fcb3d1e7c5e6
nurzaman-now <imannj69@gmail.com>
2022-12-09 15:07:02 +0700

    edit config

--- src/global/config.js
+++ src/global/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-  BASE_IMAGE: 'https://ecs-api.learnforfuture.id/storage'
+  // BASE_IMAGE: 'https://ecs-api.learnforfuture.id/storage'
+  BASE_IMAGE: 'http://localhost:8000/storage'
 }
 
 export default CONFIG;
